35de58421dcba8596a9ac1e55d557defdda8314e
Gil <gilbertjvirgo@gmail.com>
2020-09-22 18:31:00 +0100

    Changed images on about page

---
+++ src/content/testimonials.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    author: "Rev Dr Rowan Williams",
+    picture:
+      "https://images.prismic.io/socl-api/1ee449a0-40fe-4d95-aceb-be60894e4fee_rowan-williams.jpg?auto=compress,format",
+    paragraphs: [
+      "I am delighted that there is a concrete opportunity for developing this new initiative in seeking to open doors into Christian wisdom for those who may have little previous awareness of its resources.",
+      'The proposed development will be a chance for Christians of all traditions to discover new openings for growth in contemplative maturity, and I hope and pray that these plans will come to fruition."',
+    ],
+    image: require("../assets/images/bowl.png"),
+  },
+  {
+    author: "Fr Martin Laird OSA",
+    picture:
+      "https://images.prismic.io/socl-api/5918887f-2025-4078-929e-e4062f15bdcd_liard.jpg?auto=compress,format",
+    paragraphs: [
+      "The School of Contemplative Life will surely rate among the most significant responses to the spiritual longings of the people of this nation and abroad.",
+      "While we continue to see fewer and fewer people regularly attending church services, their deep, genuine longing for the depth-dimension of Christianity has only grown through the years. Sadly, most Christians have absolutely no idea that Christianity has anything to say about this, much less that it has its own vibrant contemplative tradition.",
+      "This is largely due to the fact that it has never been presented to them through schools and parishes. The School will directly nourish and cultivate the depth-dimension of life through its well-thought out programme of retreats, guidance and other opportunities to assist the Christian community to the wellspring of silence and compassion flowing within.",
+      'Quite simply, there is no other centre in this country dedicated to cultivating the human discovery of its groundless Ground, that eternal Mystery ever hidden with Christ in God."',
+    ],
+    image: require("../assets/images/jesus.png"),
+  },
+];
--- src/pages/About/Quotes.js
+++ src/pages/About/Quotes.js
@@ -1,42 +1,43 @@
+import styled, { css } from "styled-components";
+
 //Import from package.json
-import React from 'react'
-import styled, {css} from 'styled-components'
+import React from "react";
+import Section from "@component/Section";
 //Import from components
-import Text from '@component/Text'
-import Section from '@component/Section'
-import testimonials from '@content/testimonials'
+import Text from "@component/Text";
+import testimonials from "@content/testimonials";
 //import from theme
-import theme from '@/theme'
+import theme from "@/theme";
+
 const breakpoint = theme.breakpoint;
 
-const mountain='https://images.unsplash.com/photo-1579802063117-87cd2ad50a88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
-
-
+const mountain =
+  "https://images.unsplash.com/photo-1579802063117-87cd2ad50a88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 //Styling for the image in testimonials
 const Image = styled(Section.Image)`
   display: none;
-  ${breakpoint('md')`display: block`};
-`
+  ${breakpoint("md")`display: block`};
+`;
 
 //Styling for profile picture wrapper
 const Profile = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
-  ${breakpoint('md') `max-width: 65%`};
+  ${breakpoint("md")`max-width: 65%`};
   letter-spacing: 0.5;
   word-spacing: 1;
   margin-top: 1.5rem;
-`
+`;
 
 //Quote marks for the testimonials
 const Quote1 = styled.span`
   float: left;
   padding-top: 0.47rem;
   margin: -0.5rem;
-  font-size: ${props => props.theme.size.medium};
-`
+  font-size: ${(props) => props.theme.size.medium};
+`;
 
 //Styling for the profile picture
 Profile.picture = styled.div`
@@ -47,44 +48,48 @@ Profile.picture = styled.div`
   max-height: 4.4rem;
   margin-right: 1rem;
   background-size: cover;
-  background-image: url(${props => props.src});
+  background-image: url(${(props) => props.src});
   border-radius: 50%;
   display: inline-block;
-`
+`;
 
 //Grid for the testimonial section
 const Quote = styled(Section)`
-  ${breakpoint('md')`
-    ${props => (props.index % 2) && css`
-      grid-template-areas: 'image image image image image image . body body body body .'!important;
-    `}
+  ${breakpoint("md")`
+    ${(props) =>
+      props.index % 2 &&
+      css`
+        grid-template-areas: "image image image image image image . body body body body ." !important;
+      `}
 
-    padding-top: ${props => (props.index > 0) && '6rem'};
+    padding-top: ${(props) => props.index > 0 && "6rem"};
   `}
-`
+`;
 
-
-export default props => (
+export default (props) => (
   <div>
     {testimonials.map((current, index) => (
-      <Quote nopad='true' index={index}>
-        <Image src={mountain}/>
+      <Quote nopad="true" index={index}>
+        <Image src={current.image} />
 
         <Section.Body>
-
           <Section.Text>
-            <Section.Heading>{!index && 'Support for the School'}</Section.Heading>
+            <Section.Heading>
+              {!index && "Support for the School"}
+            </Section.Heading>
             <Section.Title></Section.Title>
             <Quote1>&quot;</Quote1>
-            {current.paragraphs.map(text => <p>{text}</p>)}
+            {current.paragraphs.map((text) => (
+              <p>{text}</p>
+            ))}
           </Section.Text>
 
           <Profile>
-            <Profile.picture src={current.picture}/>
+            <Profile.picture src={current.picture} />
             <Text.Sans>{current.author}</Text.Sans>
           </Profile>
         </Section.Body>
       </Quote>
     ))}
   </div>
-)
+);
