49be5c077f6758649cbd9e00340a83b7f0266104
Gil <gilbertjvirgo@gmail.com>
2020-09-24 12:20:35 +0100

    Added new image to calendar page

--- src/pages/Calendar/Overview.js
+++ src/pages/Calendar/Overview.js
@@ -1,22 +1,31 @@
 //Import from package.json
-import React from 'react'
+import React from "react";
 //Import from components
-import Section from '@component/Section'
-
+import Section from "@component/Section";
 
 //Crafting the section for the calendar page
-export default props => (
-  <Section nopad='false'>
-    <Section.Image src='https://www.welikela.com/wp-content/uploads/2015/07/santa-monica-mountains.jpg'/>
+export default (props) => (
+  <Section nopad="false">
+    <Section.Image
+      style={{ maxHeight: "300px !important" }}
+      src={require("../../assets/images/people.jpg")}
+    />
     <Section.Body>
-
       <Section.Text>
-        <Section.Heading>What do one of these events look like?</Section.Heading>
+        <Section.Heading>
+          What do one of these events look like?
+        </Section.Heading>
         <Section.Title></Section.Title>
-        <p>The School for Contemplative Life offers events and programmes that teach contemplative practice for compassionate living and social action. You can participate either by attending in-person events or online. Webcasts are available for some events which can be streamed live and for a limited time following the event.</p>
+        <p>
+          The School for Contemplative Life offers events and programmes that
+          teach contemplative practice for compassionate living and social
+          action. You can participate either by attending in-person events or
+          online. Webcasts are available for some events which can be streamed
+          live and for a limited time following the event.
+        </p>
       </Section.Text>
 
-      <Section.Link to='#'></Section.Link>
+      <Section.Link to="#"></Section.Link>
     </Section.Body>
   </Section>
-)
+);
