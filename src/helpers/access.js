00e79453abf77118de95193e3ede7232a5806f54
Gil <gilbertjvirgo@gmail.com>
2020-09-28 14:46:53 +0100

    Added sorting to access function

--- src/helpers/access.js
+++ src/helpers/access.js
@@ -1,20 +1,23 @@
-import Prismic from 'prismic-javascript'
-
-
+import Prismic from "prismic-javascript";
 
 // prismic read token.
 
-const token = 'MC5YdW5kTEJFQUFDSUF2Zm1o.W--_vQHvv73vv70dR--_vXRB77-9cgzvv73vv73vv70KVe-_vQIPZO-_vSQm77-977-9Nu-_ve-_vUPvv70'
-const endpoint = 'https://socl-api.cdn.prismic.io/api/v2'
-
-
+const token =
+  "MC5YdW5kTEJFQUFDSUF2Zm1o.W--_vQHvv73vv70dR--_vXRB77-9cgzvv73vv73vv70KVe-_vQIPZO-_vSQm77-977-9Nu-_ve-_vUPvv70";
+const endpoint = "https://socl-api.cdn.prismic.io/api/v2";
 
 // fetch records for a given content type.
 
-export default async type => {
-  let client = Prismic.client(endpoint, {accessToken: token})
-  let response = await client.query(Prismic.Predicates.at("document.type", type))
+export default async (type) => {
+  let client = Prismic.client(endpoint, { accessToken: token });
+  let response = await client.query(
+    Prismic.Predicates.at("document.type", type)
+  );
   if (response) {
-    return response.results
+    console.log(response.results);
+
+    return response.results.sort(
+      (a, b) => new Date(b.data.date) - new Date(a.data.date)
+    );
   }
-}
+};
